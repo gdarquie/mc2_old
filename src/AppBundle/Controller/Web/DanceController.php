fb0183f897a935a121d272eb6292dc87165c621c
gaetan <gaetan.darquie@gmail.com>
2016-12-01 02:52:45 +0100

    Version 0.2.12 : travail sur les pages dance, améliorations de dance, de dance content, dancing type et dance subgenre, création d'une page item pour croiser un subgenre avec un item, suppression de la partie new sur la page d'accueil

--- src/AppBundle/Controller/Web/DanceController.php
+++ src/AppBundle/Controller/Web/DanceController.php
@@ -73,9 +73,7 @@ class DanceController extends Controller
         $numberDancedNumber2 = $query->getSingleResult();
 
         //average number of shots by year
-        $query = $em -> createQuery('SELECT AVG((n.length)/n.shots) as nb, f.released as released FROM AppBundle:Number n JOIN n.film f WHERE n.shots > 0 GROUP BY f.released');
-//        $query->setParameter('instru', 'instrumental+dance');
-//        $query->setParameter('song', 'song+dance');
+        $query = $em -> createQuery('SELECT AVG((n.length)/n.shots) as nb, f.released as released FROM AppBundle:Number n JOIN n.film f WHERE n.shots > 0 AND f.released > 0 GROUP BY f.released');
         $avgNbShots = $query->getResult();
 
 //        //average number of shots by year for danced number
@@ -128,11 +126,29 @@ class DanceController extends Controller
         $query->setParameter('dance', $dance);
         $dancingTypeByYear = $query->getResult();
 
+        //numbers
+        $query = $em->createQuery('SELECT n FROM AppBundle:Number n JOIN n.dancingType d JOIN n.film f WHERE d.thesaurusId = :dance ORDER BY f.released ASC');
+        $query->setParameter('dance', $dance);
+        $numbers = $query->getResult();
+
+        //ensembles
+        $query = $em->createQuery('SELECT s.title as title, s.thesaurusId as thesaurusId, COUNT(d.thesaurusId) as nb  FROM AppBundle:Number n JOIN n.dancemble s JOIN n.dancingType d WHERE d.thesaurusId = :dance GROUP BY s.title ORDER BY nb DESC');
+        $query->setParameter('dance', $dance);
+        $dancembles = $query->getResult();
+
+        //exoticisms
+        $query = $em->createQuery('SELECT s.title as title, s.thesaurusId as thesaurusId, COUNT(d.thesaurusId) as nb  FROM AppBundle:Number n JOIN n.exoticism_thesaurus s JOIN n.dancingType d WHERE d.thesaurusId = :dance GROUP BY s.title ORDER BY nb DESC');
+        $query->setParameter('dance', $dance);
+        $exoticisms = $query->getResult();
+
         return $this->render('web/dance/dancingType.html.twig', array(
             'filmsByDance' => $filmsByDance,
             'myDance' => $myDance,
             'genres' => $genres,
-            'dancingTypeByYear' => $dancingTypeByYear
+            'dancingTypeByYear' => $dancingTypeByYear,
+            'numbers' => $numbers,
+            'dancembles' => $dancembles,
+            'exoticisms' => $exoticisms
         ));
 
     }
@@ -159,18 +175,34 @@ class DanceController extends Controller
         $query->setParameter('dance', $dance);
         $genres = $query->getResult();
 
-
         //DancingTypeByYear
         $query = $em->createQuery('SELECT f.released as released, COUNT(d.title) as nb FROM AppBundle:Number n JOIN n.danceContent d JOIN n.film f WHERE d.thesaurusId = :dance GROUP BY f.released ');
         $query->setParameter('dance', $dance);
         $danceContentByYear = $query->getResult();
 
+        //numbers
+        $query = $em->createQuery('SELECT n FROM AppBundle:Number n JOIN n.danceContent d JOIN n.film f WHERE d.thesaurusId = :dance ORDER BY f.released ASC');
+        $query->setParameter('dance', $dance);
+        $numbers = $query->getResult();
+
+        //ensembles
+        $query = $em->createQuery('SELECT s.title as title, s.thesaurusId as thesaurusId, COUNT(d.thesaurusId) as nb  FROM AppBundle:Number n JOIN n.dancemble s JOIN n.danceContent d WHERE d.thesaurusId = :dance GROUP BY s.title ORDER BY nb DESC');
+        $query->setParameter('dance', $dance);
+        $dancembles = $query->getResult();
+
+        //exoticisms
+        $query = $em->createQuery('SELECT s.title as title, s.thesaurusId as thesaurusId, COUNT(d.thesaurusId) as nb  FROM AppBundle:Number n JOIN n.exoticism_thesaurus s JOIN n.danceContent d WHERE d.thesaurusId = :dance GROUP BY s.title ORDER BY nb DESC');
+        $query->setParameter('dance', $dance);
+        $exoticisms = $query->getResult();
 
         return $this->render('web/dance/danceContent.html.twig', array(
             'filmsByDance' => $filmsByDance,
             'myDance' => $myDance,
             'genres' => $genres,
-            'danceContentByYear' => $danceContentByYear
+            'danceContentByYear' => $danceContentByYear,
+            'numbers' => $numbers,
+            'dancembles' => $dancembles,
+            'exoticisms' => $exoticisms
 
         ));
     }
@@ -183,12 +215,12 @@ class DanceController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         //filmsByDance
-        $query = $em -> createQuery('SELECT DISTINCT(f.title) as title, f.filmId as filmId, f.idImdb as imdb FROM AppBundle:Film f JOIN f.numbers n JOIN n.danceSubgenre d WHERE d.thesaurusId = :dance');
+        $query = $em -> createQuery('SELECT DISTINCT(f.title) as title, f.filmId as filmId, f.idImdb as imdb, f.released as released FROM AppBundle:Film f JOIN f.numbers n JOIN n.danceSubgenre d WHERE d.thesaurusId = :dance ORDER BY f.released ASC');
         $query->setParameter('dance', $dance);
         $filmsByDance = $query->getResult();
 
         //myDance
-        $query = $em->createQuery('SELECT DISTINCT(d.title) as title FROM AppBundle:Film f JOIN f.numbers n JOIN n.danceSubgenre d WHERE d.thesaurusId = :dance');
+        $query = $em->createQuery('SELECT DISTINCT(d.title) as title, d.thesaurusId as thesaurusId FROM AppBundle:Film f JOIN f.numbers n JOIN n.danceSubgenre d WHERE d.thesaurusId = :dance');
         $query->setParameter('dance', $dance);
         $myDance = $query->getSingleResult();
 
@@ -228,7 +260,7 @@ class DanceController extends Controller
         $exoticisms = $query->getResult();
 
         //films and numbers
-        $query = $em->createQuery('SELECT n.title as number, f.title as film FROM AppBundle:Number n JOIN n.film f JOIN n.danceSubgenre d WHERE d.thesaurusId = :dance');
+        $query = $em->createQuery('SELECT n.title as number, n.numberId as numberId, f.title as film FROM AppBundle:Number n JOIN n.film f JOIN n.danceSubgenre d WHERE d.thesaurusId = :dance');
         $query->setParameter('dance', $dance);
         $numbers = $query->getResult();
 
@@ -245,6 +277,117 @@ class DanceController extends Controller
             'exoticisms' => $exoticisms,
             "numbers" => $numbers
 
+        ));
+
+    }
+
+    /**
+     * @Route("/dance/dance-subgenre/{subgenreId}/{type}/{itemId}", name="getOneItemFromDanceSubgenre")
+     */
+    public function getOneSubgenre($subgenreId, $type, $itemId){
+
+        $em = $this->getDoctrine()->getManager();
+
+        //List of numbers
+        if($type == "dancing"){
+            $query = $em->createQuery('SELECT n FROM AppBundle:Number n JOIN n.dancingType d JOIN n.danceSubgenre s WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "content"){
+            $query = $em->createQuery('SELECT n FROM AppBundle:Number n JOIN n.danceContent d JOIN n.danceSubgenre s WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "ensemble"){
+            $query = $em->createQuery('SELECT n FROM AppBundle:Number n JOIN n.dancemble d JOIN n.danceSubgenre s WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "costume"){
+            $query = $em->createQuery('SELECT n FROM AppBundle:Number n JOIN n.costumes d JOIN n.danceSubgenre s WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "exoticism"){
+            $query = $em->createQuery('SELECT n FROM AppBundle:Number n JOIN n.exoticism_thesaurus d JOIN n.danceSubgenre s WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        $general = $query->getResult();
+
+        //Subgenre
+        $query = $em->createQuery('SELECT t FROM AppBundle:Thesaurus t WHERE t.thesaurusId = :subgenreId');
+        $query->setParameter('subgenreId', $subgenreId);
+        $subgenre = $query->getSingleResult();
+
+        //Item
+        $query = $em->createQuery('SELECT t FROM AppBundle:Thesaurus t WHERE t.thesaurusId = :itemId');
+        $query->setParameter('itemId', $itemId);
+        $item = $query->getSingleResult();
+
+        //Number of items by year
+        if($type == "dancing"){
+            $query = $em->createQuery('SELECT f.released as released, COUNT(d.thesaurusId) as nb FROM AppBundle:Number n JOIN n.dancingType d JOIN n.danceSubgenre s JOIN n.film f WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY f.released ORDER by f.released ASC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "content"){
+            $query = $em->createQuery('SELECT f.released as released, COUNT(d.thesaurusId) as nb FROM AppBundle:Number n JOIN n.danceContent d JOIN n.danceSubgenre s JOIN n.film f WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY f.released ORDER by f.released ASC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "ensemble"){
+            $query = $em->createQuery('SELECT f.released as released, COUNT(d.thesaurusId) as nb FROM AppBundle:Number n JOIN n.dancemble d JOIN n.danceSubgenre s JOIN n.film f WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY f.released ORDER by f.released ASC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "costume"){
+            $query = $em->createQuery('SELECT f.released as released, COUNT(d.thesaurusId) as nb FROM AppBundle:Number n JOIN n.costumes d JOIN n.danceSubgenre s JOIN n.film f WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY f.released ORDER by f.released ASC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "exoticism"){
+            $query = $em->createQuery('SELECT f.released as released, COUNT(d.thesaurusId) as nb FROM AppBundle:Number n JOIN n.exoticism_thesaurus d JOIN n.danceSubgenre s JOIN n.film f WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY f.released ORDER by f.released ASC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        $itemsByYear = $query->getResult();
+
+        //Performers
+        if($type == "dancing"){
+            $query = $em->createQuery('SELECT p.name as name, COUNT(p.name) as nb, p.gender as gender FROM AppBundle:Number n JOIN n.dancingType d JOIN n.danceSubgenre s JOIN n.performers p WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY p.name ORDER BY nb DESC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "content"){
+            $query = $em->createQuery('SELECT p.name as name, COUNT(p.name) as nb, p.gender as gender FROM AppBundle:Number n JOIN n.danceContent d JOIN n.danceSubgenre s JOIN n.performers p WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY p.name ORDER BY nb DESC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "ensemble"){
+            $query = $em->createQuery('SELECT p.name as name, COUNT(p.name) as nb, p.gender as gender FROM AppBundle:Number n JOIN n.dancemble d JOIN n.danceSubgenre s JOIN n.performers p WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY p.name ORDER BY nb DESC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "costume"){
+            $query = $em->createQuery('SELECT p.name as name, COUNT(p.name) as nb, p.gender as gender FROM AppBundle:Number n JOIN n.costumes d JOIN n.danceSubgenre s JOIN n.performers p WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY p.name ORDER BY nb DESC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        else if($type == "exoticism"){
+            $query = $em->createQuery('SELECT p.name as name, COUNT(p.name) as nb, p.gender as gender FROM AppBundle:Number n JOIN n.exoticism_thesaurus d JOIN n.danceSubgenre s JOIN n.performers p WHERE d.thesaurusId = :itemId AND s.thesaurusId = :subgenreId GROUP BY p.name ORDER BY nb DESC');
+            $query->setParameter('itemId', $itemId);
+            $query->setParameter('subgenreId', $subgenreId);
+        }
+        $performers = $query->getResult();
+
+        return $this->render('web/dance/item.html.twig', array(
+            'general' => $general,
+            'subgenre' => $subgenre,
+            'item' => $item,
+            'itemsByYear' => $itemsByYear,
+            'performers' => $performers
         ));
 
     }
