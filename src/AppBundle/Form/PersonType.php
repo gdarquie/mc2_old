fd4032fd2e4ac7ea863334b309cdb50ecc1fdf90
gaetan <gaetan.darquie@gmail.com>
2017-03-12 16:23:55 +0100

    Changement mineur - ajout de other pour gender

--- src/AppBundle/Form/PersonType.php
+++ src/AppBundle/Form/PersonType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 use AppBundle\Entity\Person;
@@ -26,7 +27,8 @@ class PersonType extends AbstractType
             ->add('gender', ChoiceType::class, [
                 'choices' =>[
                     "Female" => "F",
-                    "Male" => "M"
+                    "Male" => "M",
+                    "Other" => "O",
                 ]
             ])
             ->add('race')
