2425ac8679f4895140efbd23adc63acc3ea662d1
gaetan <gaetan.darquie@gmail.com>
2017-03-19 22:57:27 +0100

    Mise à jour du modèle de données

--- app/DoctrineMigrations/Version20170319225523.php
+++ app/DoctrineMigrations/Version20170319225523.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170319202746 extends AbstractMigration
+class Version20170319225523 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,9 +18,7 @@ class Version20170319202746 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE stageshow_has_film (film_id INT NOT NULL, stageShow_id INT NOT NULL, INDEX IDX_FC291A607AC68360 (stageShow_id), INDEX IDX_FC291A60567F5183 (film_id), PRIMARY KEY(stageShow_id, film_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE stageshow_has_film ADD CONSTRAINT FK_FC291A607AC68360 FOREIGN KEY (stageShow_id) REFERENCES stageShow (stageShow_id)');
-        $this->addSql('ALTER TABLE stageshow_has_film ADD CONSTRAINT FK_FC291A60567F5183 FOREIGN KEY (film_id) REFERENCES film (film_id)');
+        $this->addSql('ALTER TABLE person DROP race');
     }
 
     /**
@@ -31,6 +29,6 @@ class Version20170319202746 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE stageshow_has_film');
+        $this->addSql('ALTER TABLE person ADD race VARCHAR(45) DEFAULT NULL COLLATE utf8_general_ci');
     }
 }
