714c9e827e11caa20242427fd69b6a1129a9798d
gaetan <gaetan.darquie@gmail.com>
2017-05-21 19:21:53 +0200

    Page topics and exoticism for a person

--- src/AppBundle/Controller/PersonController.php
+++ src/AppBundle/Controller/PersonController.php
@@ -29,13 +29,179 @@ class PersonController extends Controller
      */
     public function getOnePerson($personId){
 
+        //Get Person
         $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery("SELECT p FROM AppBundle:Person p WHERE p.personId = :personId");
-        $query->setParameter('personId', $personId);
+        $query = $em->createQuery("SELECT p FROM AppBundle:Person p WHERE p.personId = :person");
+        $query->setParameter('person', $personId);
         $person = $query->getSingleResult();
 
+        //Get all numbers for the person
+        $query = $em->createQuery("SELECT n FROM AppBundle:Number n JOIN n.performers p WHERE p.personId = :person");
+        $query->setParameter('person', $personId);
+        $numbers = $query->getResult();
+
+        //Get all Performances
+        $query = $em->createQuery("SELECT COUNT(n) as nb, p.title FROM AppBundle:Number n JOIN n.performance_thesaurus p GROUP BY p.title ");
+        $performances = $query->getResult();
+
+        //Get a Performance
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.performance_thesaurus t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ");
+        $query->setParameter('person', $personId );
+        $performance = $query->getResult();
+
+        //Get all structures
+        $query = $em->createQuery("SELECT COUNT(DISTINCT(n.numberId)) as nb, t.title FROM AppBundle:Number n JOIN n.structure t JOIN n.performers p GROUP BY t.title");
+        $structures = $query->getResult();
+
+        //Get total of numbers [vérifier]
+        $query = $em->createQuery("SELECT COUNT(DISTINCT(n.numberId)) as total FROM AppBundle:Number n JOIN n.structure t JOIN n.performers p");
+        $structures_total = $query->getSingleResult();
+
+        //Get ????
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.structure t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ");
+        $query->setParameter('person', $personId );
+        $structure = $query->getResult();
+
+//        Topics part (genre, general_mood)
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.genre t JOIN n.performers p GROUP BY t.title ORDER BY nb DESC");
+        $genres = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.genre t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $genre = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.general_mood t JOIN n.performers p GROUP BY t.title ORDER BY nb DESC");
+        $moods = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.general_mood t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $mood = $query->getResult();
+
+//      Exoticism
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.exoticism_thesaurus t JOIN n.performers p GROUP BY t.title ORDER BY nb DESC");
+        $exoticisms = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.exoticism_thesaurus t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $exoticism = $query->getResult();
+
+//       Sources
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.source_thesaurus t JOIN n.performers p GROUP BY t.title ORDER BY nb DESC");
+        $sources = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.source_thesaurus t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $source = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.dancingType t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $dancing = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.musical_thesaurus t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $musical = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.completenessThesaurus t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $completeness = $query->getResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb FROM AppBundle:Number n JOIN n.completenessThesaurus t JOIN n.performers p WHERE p.personId = :person");
+        $query->setParameter('person', $personId );
+        $completes = $query->getSingleResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb FROM AppBundle:Number n JOIN n.completenessThesaurus t JOIN n.performers p WHERE p.personId = :person AND t.title = :complete");
+        $query->setParameter('person', $personId );
+        $query->setParameter('complete', 'complete' );
+        $complete = $query->getSingleResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb FROM AppBundle:Number n JOIN n.completOptions t JOIN n.performers p WHERE p.personId = :person AND t.title = :occurences");
+        $query->setParameter('person', $personId );
+        $query->setParameter('occurences', 'multiple occurrences of a song or partial reprise' );
+        $occurences = $query->getSingleResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb FROM AppBundle:Number n JOIN n.completOptions t JOIN n.performers p WHERE p.personId = :person");
+        $query->setParameter('person', $personId );
+        $completOptions = $query->getSingleResult();
+
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.diegetic_thesaurus t JOIN n.performers p GROUP BY t.title ORDER BY nb DESC");
+        $diegetics = $query->getResult();
+
+        //
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.diegetic_thesaurus t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $diegetic = $query->getResult();
+
+        //
+        $query = $em->createQuery("SELECT DISTINCT(f.filmId) as filmId FROM AppBundle:Number n JOIN n.film f JOIN n.performers p WHERE p.personId = :person");
+        $query->setParameter('person', $personId );
+        $filmsWithPerson = $query->getResult();
+//        $filmsWithPerson = [4349,4606,4690,5030];
+
+        //total des durées des numbers pour chaque film avec person (pourquoi HAVING marche pas)
+        $query = $em->createQuery("SELECT SUM((n.endTc - n.beginTc)) as total, f.length as length, f.title as title, f.released FROM AppBundle:Film f JOIN f.numbers n WHERE f.filmId IN (:film) GROUP BY f.filmId ORDER BY f.released ASC");
+//        $query->setParameter('person', $name );
+        $query->setParameter('film', $filmsWithPerson );
+        $lengthTotal = $query->getResult();
+
+        $query = $em->createQuery("SELECT SUM((n.endTc - n.beginTc)) as total, f.title as title FROM AppBundle:Number n JOIN n.performers p JOIN n.film f WHERE p.personId = :person  AND f.filmId IN (:film) GROUP BY f.filmId ORDER BY f.released ASC");
+        $query->setParameter('person', $personId );
+        $query->setParameter('film', $filmsWithPerson );
+        $lengthTotalForPerson = $query->getResult();
+
+        if(count($lengthTotalForPerson) == count($lengthTotal)) {
+
+            $ratio = [];
+            for ($i = 0; $i < count($lengthTotalForPerson); $i++) {
+
+                array_push($ratio, array("all" => $lengthTotal[$i]['total'], "title" => $lengthTotal[$i]['title'], "length" => $lengthTotal[$i]['length'], "released" => $lengthTotal[$i]['released'], "one" => $lengthTotalForPerson[$i]['total'] ));
+            }
+        }
+
+        //durée moyenne pour d'un shot pour un number
+        $query = $em->createQuery("SELECT DISTINCT(n.numberId), AVG(n.shots) as average FROM AppBundle:Number n JOIN n.performers p WHERE p.personId = :person");
+        $query->setParameter('person', $personId );
+        $avgShotForPerson = $query->getResult();
+        //rapporter par rapport à la durée de plans
+
+
+        //une moyenne par films
+        $query = $em->createQuery("SELECT COUNT(n) as nb, t.title FROM AppBundle:Number n JOIN n.diegetic_thesaurus t JOIN n.performers p WHERE p.personId = :person GROUP BY t.title ORDER BY nb DESC");
+        $query->setParameter('person', $personId );
+        $presence = $query->getResult();
+
         return $this->render('web/person/person.html.twig', array(
-            'person' => $person
+            'person' => $person,
+            'numbers' => $numbers,
+            'performances' => $performances,
+            'performance' => $performance,
+            'structure' => $structure,
+            'structures' => $structures,
+            'genres' => $genres,
+            'genre' => $genre,
+            'source' => $source,
+            'sources' => $sources,
+            'dancing' => $dancing,
+            'musical' => $musical,
+            'completeness' => $completeness,
+            'completes' => $completes,
+            'complete' => $complete,
+            'occurences' => $occurences,
+            'completOptions' => $completOptions,
+            'diegetic' => $diegetic,
+            'diegetics' => $diegetics,
+            'lengthTotal' => $lengthTotal,
+            'filmsWithPerson' => $filmsWithPerson,
+            'lengthTotalForPerson' =>$lengthTotalForPerson,
+            'ratio' => $ratio,
+            'structures_total' => $structures_total,
+            'avgShotForPerson' => $avgShotForPerson,
+            'moods' => $moods,
+            'mood' => $mood,
+            'exoticisms' => $exoticisms,
+            'exoticism' => $exoticism
         ));
     }
 
--- web/js/mc2.js
+++ web/js/mc2.js
@@ -44,6 +44,15 @@ jQuery.fn.displaytable = function() {
 
 };
 
+$('.modal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%', // Ending top style attribute
+    }
+);
 
 $(document).ready(function() {
 	myParallax();
@@ -75,7 +84,9 @@ $(document).ready(function() {
 
         console.log("dtable = "+dtable);
         $(".displaytable").displaytable();
-    })
+    });
+
+
 
 });
 
